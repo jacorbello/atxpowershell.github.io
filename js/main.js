dd67c71644d7c941cf211206d372500ccb9234ff
Jeremy Corbello <Jeremy@jeremycorbello.com>
2019-02-16 09:37:11 -0600

    Updated with connect section as well as start of apil to meetup for cal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', "*");
+    xmlHttp.send(null);
+}
+
 function showContent(i) {
     var divs = [
         "mainWindow",
         "eventsDiv",
         "projectsDiv",
         "resourcesDiv",
+        "connectDiv"
     ]
     
 
@@ -30,4 +43,19 @@ function showContent(i) {
         });
     }
 
+}
+
+function getMeetup() {
+    // TODO Figure out why I'm getting this message when querying the meetup API: 
+    // Access to XMLHttpRequest at 'https://api.meetup.com/Austin-Powershell/events?photo-host=public&page=20&sig_id=238371648&sig=8bc229342e4f64c52f391ad67bf4359fe9d2f40f' from origin 'null' has been blocked by CORS policy: 
+    // Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+    console.log("Called getMeetup()");
+    url = "https://api.meetup.com/Austin-Powershell/events?photo-host=public&page=20&sig_id=238371648&sig=8bc229342e4f64c52f391ad67bf4359fe9d2f40f"
+    httpGetAsync(url, function(e) {
+        console.log(e);
+    });
+
+
+    // https://api.meetup.com/Austin-Powershell/events?&sign=true&photo-host=public&page=20
+    // https://api.meetup.com/Austin-Powershell/events?photo-host=public&page=20&sig_id=238371648&sig=8bc229342e4f64c52f391ad67bf4359fe9d2f40f
 }
